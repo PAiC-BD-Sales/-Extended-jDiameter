dd326792785ecfb02e0627a03a17624bae7d2235
Kenny Mendieta <kenny.mendieta@paicbd.com>
2022-08-05 17:21:01 -0600

    Added the events short, long names and code. EJD-43

--- core/jdiameter/api/src/main/java/org/jdiameter/api/zh/events/MultimediaAuthRequest.java
+++ core/jdiameter/api/src/main/java/org/jdiameter/api/zh/events/MultimediaAuthRequest.java
@@ -3,4 +3,8 @@ package org.jdiameter.api.zh.events;
 import org.jdiameter.api.app.AppRequestEvent;
 
 public interface MultimediaAuthRequest extends AppRequestEvent {
+    String _SHORT_NAME = "MAR";
+    String _LONG_NAME = "Multimedia-Auth-Request";
+
+    int code = 303;
 }

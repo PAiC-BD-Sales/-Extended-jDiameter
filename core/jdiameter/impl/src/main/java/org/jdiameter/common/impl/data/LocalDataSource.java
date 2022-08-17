9318bcdd56bf32cffcd4bf5a505a37b99a3d592e
Kenny Mendieta <kenny.mendieta@paicbd.com>
2022-08-17 17:50:59 -0600

    Added ZhLocalSessionDataFactory in LocalDataSource constructor. EDRA-85

--- core/jdiameter/impl/src/main/java/org/jdiameter/common/impl/data/LocalDataSource.java
+++ core/jdiameter/impl/src/main/java/org/jdiameter/common/impl/data/LocalDataSource.java
@@ -64,6 +64,7 @@ import org.jdiameter.common.api.app.s6a.IS6aSessionData;
 import org.jdiameter.common.api.app.sh.IShSessionData;
 import org.jdiameter.common.api.app.slg.ISLgSessionData;
 import org.jdiameter.common.api.app.slh.ISLhSessionData;
+import org.jdiameter.common.api.app.zh.IZhSessionData;
 import org.jdiameter.common.api.data.ISessionDatasource;
 import org.jdiameter.common.impl.app.acc.AccLocalSessionDataFactory;
 import org.jdiameter.common.impl.app.auth.AuthLocalSessionDataFactory;
@@ -78,6 +79,7 @@ import org.jdiameter.common.impl.app.s6a.S6aLocalSessionDataFactory;
 import org.jdiameter.common.impl.app.sh.ShLocalSessionDataFactory;
 import org.jdiameter.common.impl.app.slg.SLgLocalSessionDataFactory;
 import org.jdiameter.common.impl.app.slh.SLhLocalSessionDataFactory;
+import org.jdiameter.common.impl.app.zh.ZhLocalSessionDataFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -111,6 +113,7 @@ public class LocalDataSource implements ISessionDatasource {
     appSessionDataFactories.put(IS13SessionData.class, new S13LocalSessionDataFactory());
     appSessionDataFactories.put(ISLhSessionData.class, new SLhLocalSessionDataFactory());
     appSessionDataFactories.put(ISLgSessionData.class, new SLgLocalSessionDataFactory());
+    appSessionDataFactories.put(IZhSessionData.class, new ZhLocalSessionDataFactory());
   }
 
   public LocalDataSource(IContainer container) {

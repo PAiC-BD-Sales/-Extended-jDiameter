60f6b4d8d10b68a5ce23d3688d3d5017f421486b
Kenny Mendieta <kenny.mendieta@paicbd.com>
2022-08-23 17:53:04 -0600

    Added hashCode method on ZhClientSessionImpl. EDRA-85

--- core/jdiameter/impl/src/main/java/org/jdiameter/client/impl/app/zh/ZhClientSessionImpl.java
+++ core/jdiameter/impl/src/main/java/org/jdiameter/client/impl/app/zh/ZhClientSessionImpl.java
@@ -227,6 +227,13 @@ public class ZhClientSessionImpl extends ZhSession
     }
   }
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + (int) (appId ^ (appId >>> 32));
+    return result;
+  }
+  @Override
   public boolean equals(Object obj) {
     if (this == obj) {
       return true;
